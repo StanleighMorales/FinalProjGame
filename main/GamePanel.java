21910fc9db7311a3c0f34feeb239675892ff0d38
Stanleigh Morales <stanleighmorales@gmail.com>
2023-12-22 01:39:29 +0800

    FPS added, with some FPS capping. Added an Image holder. added the Character Sprite.

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -14,24 +14,42 @@ import java.io.InputStream;
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
     private float xDelta = 100, yDelta = 100;
-    private BufferedImage img;
+    private BufferedImage img,subImg;
 
     private int frameRate = 0;
     private long lastCheck  = 0;
-
+    private BufferedImage[] idleAni;
     public GamePanel() {
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+
         importImg();
 
     }
+//    private void loadAnimations(){
+//        idleAni = new BufferedImage[4];
+//
+//        for(int i=0; i<idleAni.length; i++)
+//            idleAni[i] = img.getSubimage(1*20, 80,264 ,80);
+//
+//    }
 
     private void importImg() {
-        InputStream is = getClass().getResourceAsStream("/MC.jpeg");
+        InputStream inputStream = getClass().getResourceAsStream("/Idle-Sheet.png");
 
-
+        try {
+            img = ImageIO.read(inputStream);
+        } catch (IOException e) {
+              throw new RuntimeException(e);
+        }finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
     }
 
@@ -58,10 +76,14 @@ public class GamePanel extends JPanel {
     }
 
 
-    public void paintComponent(Graphics g){
-     super.paintComponent(g);
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+//        g.drawImage(img.getSubimage(0,0, 256, 80), 0,0, null);
+      subImg = img.getSubimage(3*20, 0*80, 60, 80 );
+      g.drawImage(subImg, (int)xDelta,(int) yDelta,120, 160, null);
 
-//    g.drawImage(null, x, y, null);
+//        g.drawImage(idleAni[2],(int)xDelta,(int) yDelta,120, 160, null);
+
 
     }
 
